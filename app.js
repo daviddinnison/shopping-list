c6dcb90d37e368339e0670cea0daa5c263075901
Evan Harris <echarris@smcm.edu>
2017-06-16 12:49:43 -0400

    Adds template and initial state

--- app.js
+++ app.js
@@ -7,8 +7,16 @@
 //set state to current contents for .shopping-list
 //set var for text names and checkeded true or false in object
 
-let initialState = { };
+let initialState = { 
+	items: [
+		{ name:"apples", checked: false},
+		{ name:"oranges", checked: false},
+		{ name:"milk", checked: true},
+		{ name:"bread", checked: false}
+	]
+};
 
+// console.log(initialState);
 
 //STEP TWO -------- STATE MODIFICATION FUNCTIONS
 //create functions for adding/removing from state
@@ -18,19 +26,47 @@ let initialState = { };
 
 //STEP THREE ---------- RENDER FUNCTION HANDLING
 //pushing state into DOM via append
+
+//renderfunction
+function rendersItems(state) {
 //create box with text entered
 
+	//make template 
+	let template = (`
+		<li>
+		<span class="shopping-item">${state.items.name}</span> 
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`)
+      
 
-
+ alert(template);
+}
 //STEP FOUR -------- USER ACTIONS HANDLING
 //listen for form submission click and text entered
-//store form text in variable
+function submitsForm(){
+	$('#js-shopping-list-form').submit(function(event){
+	//store form text in variable
+	let userInput = $('#shopping-list-entry').val();
+	console.log(userInput);
+	});
+}
+
 //add check functioniality via class removal/adding 
 //add delete functionality (how?)
 
 
 
 
-
 //STEP FIVE ------ INTIALIZE IT
 //doc ready w/render function
+$(function (){
+	rendersItems(initialState);
+	submitsForm();
+}) 
