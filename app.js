0b2bef436132b3b52b2497ba15df897b9e631d6f
daviddinnison <david.g.dinnison@gmail.com>
2017-06-16 11:59:08 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+//input initial state and whatever is inputted --- to do items
+//outputting to do items in DOM
+
+
+
+// STEP ONE ------- DESIGN STATE
+//set state to current contents for .shopping-list
+//set var for text names and checkeded true or false in object
+
+let initialState = { };
+
+
+//STEP TWO -------- STATE MODIFICATION FUNCTIONS
+//create functions for adding/removing from state
+//
+
+
+
+//STEP THREE ---------- RENDER FUNCTION HANDLING
+//pushing state into DOM via append
+//create box with text entered
+
+
+
+//STEP FOUR -------- USER ACTIONS HANDLING
+//listen for form submission click and text entered
+//store form text in variable
+//add check functioniality via class removal/adding 
+//add delete functionality (how?)
+
+
+
+
+
+//STEP FIVE ------ INTIALIZE IT
+//doc ready w/render function
